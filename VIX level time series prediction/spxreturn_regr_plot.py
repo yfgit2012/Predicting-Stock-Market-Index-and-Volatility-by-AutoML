d2a3358440d86c010be97d1f9a0d70ec779034bf
Yifu <fbai.cn@gmail.com>
2022-03-20 23:10:03 -0700

    update

--- VIX level time series prediction/spxreturn_regr_plot.py
+++ VIX level time series prediction/spxreturn_regr_plot.py
@@ -10,9 +10,9 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-Title = ['VIX level prediction by RF (2018-2020)', 'VIX level prediction by AB (2018-2020)',  
-         'VIX level prediction by XB (2018-2020)', 'VIX level prediction by SVM (2018-2020)',  
-         'VIX level prediction by LSTM (2018-2020)', 'VIX level prediction by AG (2018-2020)'] 
+Title = ['SPX return prediction by RF (2018-2020)', 'SPX return prediction by AB (2018-2020)',  
+         'SPX return prediction by XB (2018-2020)', 'SPX return prediction by SVM (2018-2020)',  
+         'SPX return prediction by LSTM (2018-2020)', 'SPX return prediction by AG (2018-2020)'] 
 
 
 df1 = pd.read_csv('./results/'+Title[0]+'.csv')
@@ -30,8 +30,8 @@ plt.title(Title[0])
 plt.plot(df1['actual_return'], label="label")
 plt.plot(df1['pred_return'],label="prediction")
 plt.xlabel("Data points")
-plt.ylabel("VIX Level")
-plt.text(-5, 65, 'RMSE=2.97, Signal_accuracy=55.1%', fontsize = 10)
+plt.ylabel("SPX return")
+plt.text(-5, -6, 'RMSE=1.04, Signal_accuracy=71.2%', fontsize = 10)
 plt.legend(loc="upper left")
 plt.savefig('./results/'+Title[0]+'.png')
 plt.show()
@@ -42,11 +42,12 @@ plt.title(Title[1])
 plt.plot(df2['actual_return'], label="label")
 plt.plot(df2['pred_return'],label="prediction")
 plt.xlabel("Data points")
-plt.ylabel("VIX Level")
-plt.text(-5, 65, 'RMSE=3.41, Signal_accuracy=49.7%', fontsize = 10)
+plt.ylabel("SPX return")
+plt.text(-5, -6, 'RMSE=1.04, Signal_accuracy=70.4%', fontsize = 10)
 plt.legend(loc="upper left")
 plt.savefig('./results/'+Title[1]+'.png')
 plt.show()
+
 
 #plt.subplot(2, 3, 3)
 plt.figure()
@@ -54,11 +55,12 @@ plt.title(Title[2])
 plt.plot(df3['actual_return'], label="label")
 plt.plot(df3['pred_return'],label="prediction")
 plt.xlabel("Data points")
-plt.ylabel("VIX Level")
-plt.text(-5, 65, 'RMSE=4.76, Signal_accuracy=54.8%', fontsize = 10)
+plt.ylabel("SPX return")
+plt.text(-5, -6, 'RMSE=0.87, Signal_accuracy=63.5%', fontsize = 10)
 plt.legend(loc="upper left")
 plt.savefig('./results/'+Title[2]+'.png')
 plt.show()
+
 
 #plt.subplot(2, 3, 4)
 plt.figure()
@@ -66,11 +68,12 @@ plt.title(Title[3])
 plt.plot(df4['actual_return'], label="label")
 plt.plot(df4['pred_return'],label="prediction")
 plt.xlabel("Data points")
-plt.ylabel("VIX Level")
-plt.text(-5, 65, 'RMSE=7.43, Signal_accuracy=57.0%', fontsize = 10)
+plt.ylabel("SPX return")
+plt.text(-5, -6, 'RMSE=1.03, Signal_accuracy=60.2%', fontsize = 10)
 plt.legend(loc="upper left")
 plt.savefig('./results/'+Title[3]+'.png')
 plt.show()
+
 
 #plt.subplot(2, 3, 5)
 plt.figure()
@@ -78,11 +81,12 @@ plt.title(Title[4])
 plt.plot(df5['actual_return'], label="label")
 plt.plot(df5['pred_return'],label="prediction")
 plt.xlabel("Data points")
-plt.ylabel("VIX Level")
-plt.text(-5, 65, 'RMSE=3.74, Signal_accuracy=55.8%', fontsize = 10)
+plt.ylabel("SPX return")
+plt.text(-5, -6, 'RMSE=1.17, Signal_accuracy=47.8%', fontsize = 10)
 plt.legend(loc="upper left")
 plt.savefig('./results/'+Title[4]+'.png')
 plt.show()
+
 
 #plt.subplot(2, 3, 6)
 plt.figure()
@@ -90,9 +94,10 @@ plt.title(Title[5])
 plt.plot(df6['actual_return'], label="label")
 plt.plot(df6['pred_return'],label="prediction")
 plt.xlabel("Data points")
-plt.ylabel("VIX Level")
-plt.text(-5, 65, 'RMSE=4.28, Signal_accuracy=54.6%', fontsize = 10)
+plt.ylabel("SPX return")
+plt.text(-5, -6, 'RMSE=1.00, Signal_accuracy=68.4%', fontsize = 10)
 plt.legend(loc="upper left")
 plt.savefig('./results/'+Title[5]+'.png')
 plt.show()
+
 
